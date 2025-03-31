d8442b09f56a54161b87702c02095461ff9caaad
Shantele King <Shantele.kingsch@gmail.com>
2025-03-31 10:13:03 -0400

    Added code to dbconnector

--- CooperativeCougars_Assignment09/dbconnector_Package/dbconnector.py
+++ CooperativeCougars_Assignment09/dbconnector_Package/dbconnector.py
@@ -1,1 +1,18 @@
 
+class dbconnector:
+    def connect_to_database(self):
+        '''
+            connect to out SQL Server instance 
+            @return the connection object
+            '''
+        try:
+            conn = pyodbc.connect('Driver={SQL Server};'
+            'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
+            'Database=IS4010;'
+            'uid=IS4010Login;'
+            'pwd=P@ssword2;')
+        except:
+
+           # print("Unable to connet to datebase") # Probably not wise 
+            return None
+        return conn
